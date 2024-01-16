c0fc548cb3ce5328abea8dde12bc8cdf54d81105
pytholic <rajahaseeb147@gmail.com>
2024-01-16 09:37:52 +0000

    batch deployment - score script

--- 04-deployment/batch/README.md
+++ 04-deployment/batch/README.md
@@ -7,7 +7,31 @@ The ideal way of deployment for our app would be a `web-service`. However, here 
 3. Clean and parametrize it
 
 ## Notebook conversion
-Clean and remove extra stuff from the notebook. We will load the model and just apply it.
+Clean and remove extra stuff from the notebook. We will load the model and just apply it. Check `score.ipynb`.
 
-We will create a `result` dataframe.
-- Includes `ride-id`. Since we don't have it in our original dataframe, we will add artificial ones. Normally, r`ride_ids` should already be there.
+1. We will create a `result` dataframe and save it.
+   - We will include a `ride-id` so that we can relate the predictions with the correct ride. Since we don't have it in our original dataframe, we will add artificial ones. Normally, `ride_ids` should already be there.
+   - We will also include some other information like pickup and dropoff location, actual dutation, predicted duration, and the difference between the two durations.
+
+2. Convert everything to functions.
+
+## Convert the notebook into a script.
+```
+jupyter nbconvert --to script score.ipynb
+```
+
+Clean and parameterize the script like `score.py`.
+
+Run it.
+```
+python score.py green <YEAR> <MONTH> <EXPERIMENT ID> <RUN ID>
+python score.py green 2021 2 1 553def03f5224f649fe56bc1567daccc
+```
+
+Output logs.
+```
+Reading the data from https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2021-01.parquet...
+Loading the model with RUN_ID=553def03f5224f649fe56bc1567daccc...
+Applying the model...
+Saving the result to output/green/2021-01.parquet...
+```
---
+++ 04-deployment/batch/score.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+import mlflow
+import pickle
+import pandas as pd
+import uuid
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.pipeline import make_pipeline
+
+
+service_account_key_path = os.path.expanduser('~') + "/service_account_key.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = service_account_key_path
+
+
+# Utility functions
+def gen_uuids(n):
+    ride_ids = []
+    for i in range(n):
+        ride_ids.append(str(uuid.uuid4()))
+    return ride_ids
+
+def read_dataframe(filename: str):
+    df = pd.read_parquet(filename)
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.dt.total_seconds() / 60
+    df = df[(df.duration >= 1) & (df.duration <= 60)]    
+
+    df["ride_id"] = gen_uuids(len(df))
+    return df
+
+def prepare_dictionaries(df: pd.DataFrame):
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+    df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
+    categorical = ['PU_DO']
+    numerical = ['trip_distance']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+    return dicts
+
+def load_model(experiment_id, run_id):
+    logged_model = f"gs://pytholic-mlops-zoomcamp-artifacts/{experiment_id}/{run_id}/artifacts/model"
+    model = mlflow.pyfunc.load_model(logged_model)
+    return model 
+
+def apply_model(input_file, experiment_id, run_id, output_file):
+
+    print(f"Reading the data from {input_file}...")
+    df = read_dataframe(input_file)
+    dicts = prepare_dictionaries(df)
+
+    print(f"Loading the model with RUN_ID={run_id}...")
+    model = load_model(experiment_id, run_id)
+
+    print(f"Applying the model...")
+    y_pred = model.predict(dicts)
+
+    print(f"Saving the result to {output_file}...")
+    df_result = pd.DataFrame()
+    df_result["ride_id"] = df["ride_id"]
+    df_result["lpep_pickup_datetime"] = df["lpep_pickup_datetime"]
+    df_result["PULocationID"] = df["PULocationID"]
+    df_result["DOLocationID"] = df["DOLocationID"]
+    df_result["actual_duration"] = df["duration"]
+    df_result["predicted_duration"] = y_pred
+    df_result["diff"] = df_result["actual_duration"] - df_result["predicted_duration"]
+    df_result["model_version"] = run_id
+
+    df_result.to_parquet(output_file, index=False)
+
+
+def run():
+    """
+    Main function to load model/data and apply the model.
+    """
+    year = int(sys.argv[2]) #2021
+    month = int(sys.argv[3]) # 2
+    taxi_type = sys.argv[1] #"green"
+
+    if not os.path.exists(f"output/{taxi_type}"): 
+        os.makedirs(f"output/{taxi_type}") 
+
+    input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet"  # download from link
+    output_file = f"output/{taxi_type}/{year:04d}-{month:02d}.parquet"
+
+    EXPERIMENT_ID = sys.argv[4] # 1
+    RUN_ID = sys.argv[5] # "553def03f5224f649fe56bc1567daccc"
+    logged_model = f"gs://pytholic-mlops-zoomcamp-artifacts/{EXPERIMENT_ID}/{RUN_ID}/artifacts/model"
+    
+    apply_model(input_file, EXPERIMENT_ID, RUN_ID, output_file)
+
+
+if __name__ == "__main__":
+    run()
